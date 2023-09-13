4e9d03b1c502e3e2c5bb7a7f233dc4037bae1e47
Motasem Abunima <motasemabunima@gmail.com>
2023-09-13 17:52:48 +0300

    some updates

--- cypress/e2e/new_spec.cy.js
+++ cypress/e2e/new_spec.cy.js
@@ -1,5 +1,12 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+/// <reference types="cypress" />
+
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("https://example.cypress.io");
+  });
+
+  it("fails", () => {
+    cy.visit("https://example.cypress.io");
+    cy.get("body").should("not.exist");
+  });
+});
