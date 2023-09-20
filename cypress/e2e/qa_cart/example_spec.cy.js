a36f2df1e8c593a21b313a80aadaefd0639e86bf
Motasem Abunima <motasemabunima@gmail.com>
2023-09-20 17:53:49 +0300

    expect command

--- cypress/e2e/qa_cart/example_spec.cy.js
+++ cypress/e2e/qa_cart/example_spec.cy.js
@@ -1,6 +1,10 @@
 /// <reference types="cypress" />
 describe("First Test Scenario", () => {
-  it("Individual Test Case", () => {
-    expect("true").to.be.true;
+  it("Individual Test Case I", () => {
+    expect(true).to.be.true;
+  });
+
+  it("Individual Test Case II", () => {
+    expect(false).to.be.true;
   });
 });
