47227cbdeb0ca5ee551a57649cd084a521efeabc
Motasem Abunima <motasemabunima@gmail.com>
2023-10-02 21:38:39 +0300

    log something

--- cypress/e2e/qa_cart/example_spec.cy.js
+++ cypress/e2e/qa_cart/example_spec.cy.js
@@ -1,6 +1,10 @@
 /// <reference types="cypress" />
 describe("QA Cart Test Suite", () => {
-  it("should pass", () => {
-    expect(true).to.equal(true);
+  it("should log something", () => {
+    cy.log("Hello World!");
+  });
+
+  it("should log something else", () => {
+    cy.log("Hi there!");
   });
 });
