49a83cffa95c531bf9b25136c096496c38c0d3a3
Motasem Abunima <motasemabunima@gmail.com>
2023-10-04 20:27:12 +0300

    before, beforeEach, after, afterEach

--- cypress/e2e/qa_cart/example_spec.cy.js
+++ cypress/e2e/qa_cart/example_spec.cy.js
@@ -1,5 +1,21 @@
 /// <reference types="cypress" />
 describe("QA Cart Test Suite", () => {
+  before(() => {
+    cy.log("Before all tests");
+  });
+
+  beforeEach(() => {
+    cy.log("Before each test");
+  });
+
+  afterEach(() => {
+    cy.log("After each test");
+  });
+
+  after(() => {
+    cy.log("After all tests");
+  });
+
   it("should log something", () => {
     cy.log("Hello World!");
   });
